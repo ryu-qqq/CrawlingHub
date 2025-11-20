1be7c9e09db7003d03d6721ebab1cae67cd66956
sangwon-ryu <fbtkdals2@naver.com>
2025-11-20 17:39:38 +0900

    fix: ecs-web-api outputs.tf 직접 리소스 참조로 변경

--- terraform/ecs-web-api/outputs.tf
+++ terraform/ecs-web-api/outputs.tf
@@ -4,17 +4,17 @@
 
 output "alb_dns_name" {
   description = "ALB DNS name"
-  value       = module.alb.dns_name
+  value       = aws_lb.web_api.dns_name
 }
 
 output "alb_arn" {
   description = "ALB ARN"
-  value       = module.alb.arn
+  value       = aws_lb.web_api.arn
 }
 
 output "service_name" {
   description = "ECS service name"
-  value       = module.ecs_service_web_api.service_name
+  value       = aws_ecs_service.web_api.name
 }
 
 output "fqdn" {
