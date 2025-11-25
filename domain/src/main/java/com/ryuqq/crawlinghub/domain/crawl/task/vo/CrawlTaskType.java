73c511f64e59ec23f4b61fd91d526eaeefe1a36f
sangwon-ryu <fbtkdals2@naver.com>
2025-11-25 12:41:45 +0900

    feat: CrawlTaskType Enum 구현 (Green)
    - 4개 유형: META, MINI_SHOP, DETAIL, OPTION
    - description 필드 + getDescription() 메서드

---
+++ domain/src/main/java/com/ryuqq/crawlinghub/domain/crawl/task/vo/CrawlTaskType.java
@@ -0,0 +1,46 @@
+package com.ryuqq.crawlinghub.domain.crawl.task.vo;
+
+/**
+ * CrawlTask 유형 Enum
+ *
+ * <p><strong>크롤링 유형</strong>:
+ *
+ * <ul>
+ *   <li>{@code META} - 미니샵 메타 정보 크롤링
+ *   <li>{@code MINI_SHOP} - 미니샵 상품 목록 크롤링
+ *   <li>{@code DETAIL} - 상품 상세 정보 크롤링
+ *   <li>{@code OPTION} - 상품 옵션 정보 크롤링
+ * </ul>
+ *
+ * @author development-team
+ * @since 1.0.0
+ */
+public enum CrawlTaskType {
+
+    /** 미니샵 메타 정보 크롤링 */
+    META("미니샵 메타 정보"),
+
+    /** 미니샵 상품 목록 크롤링 */
+    MINI_SHOP("미니샵 상품 목록"),
+
+    /** 상품 상세 정보 크롤링 */
+    DETAIL("상품 상세 정보"),
+
+    /** 상품 옵션 정보 크롤링 */
+    OPTION("상품 옵션 정보");
+
+    private final String description;
+
+    CrawlTaskType(String description) {
+        this.description = description;
+    }
+
+    /**
+     * 유형 설명 반환
+     *
+     * @return 유형 설명
+     */
+    public String getDescription() {
+        return description;
+    }
+}
